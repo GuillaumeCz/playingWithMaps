1d1765755e70a71210ac989e271c350767406af4
115c7a5fac <115c7a5fac@pm.me>
2024-01-26 08:13:37 +0100

    Add sampling and fix x axis of the chart

--- src/pages/LeafletPage/LeafletMapCtrl/index.tsx
+++ src/pages/LeafletPage/LeafletMapCtrl/index.tsx
@@ -1,11 +1,17 @@
 import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { getElevationProfile, getElevations } from "../../../api/altiApi";
 import { IElevation, IPoint } from "../../../types";
-import { Checkbox, FormControlLabel, FormGroup, Slider } from "@mui/material";
+import {
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  Slider,
+  TextField,
+} from "@mui/material";
 
 import "./leafletMapCtrl.css";
 import { LineChart } from "@mui/x-charts";
-import { LatLng, LatLngBounds } from "leaflet";
+import { LatLng } from "leaflet";
 
 interface ILeafletMapCtrlProps {
   points: Array<IPoint>;
@@ -13,6 +19,7 @@ interface ILeafletMapCtrlProps {
 }
 const LeafletMapCtrl = ({ points, onPointChange }: ILeafletMapCtrlProps) => {
   const [elevationProfile, setElevationProfile] = useState<IElevation[]>([]);
+  const [sampling, setSampling] = useState<number>(10);
 
   // Pas ouf mais rien trouvé de mieux pour check si ces parametres là on changés (dans le tableau d'objets)
   const locationsTrigger = points
@@ -40,19 +47,28 @@ const LeafletMapCtrl = ({ points, onPointChange }: ILeafletMapCtrlProps) => {
   useEffect(() => {
     const fetchData = async () => {
       setElevationProfile(
-        await getElevationProfile(points.map(({ location }) => location)),
+        await getElevationProfile(
+          points.map(({ location }) => location),
+          sampling,
+        ),
       );
     };
 
     fetchData().catch(console.error);
     // eslint-disable-next-line
-  }, [locationsTrigger]);
+  }, [locationsTrigger, sampling]);
+
+  const xAxis: number[] = elevationProfile
+    .map(({ lat, lng, z }, i) => new LatLng(lat, lng, z))
+    .map((formattedPoint, i, pts) =>
+      i === 0 ? 0 : Math.round(pts[0].distanceTo(formattedPoint)),
+    );
 
   // Meilleur moyen pour choper la distance ? Reflechir a faire ça en 1 expression, 1 shot
   const formattedPoints: LatLng[] = points.map(
     ({ location: { lat, lng }, elevation }) => new LatLng(lat, lng, elevation),
   );
-  const distance = formattedPoints[0].distanceTo(formattedPoints[1]);
+  const distance: number = formattedPoints[0].distanceTo(formattedPoints[1]);
 
   return (
     <div id={"leaflet-ctrl"}>
@@ -91,30 +107,44 @@ const LeafletMapCtrl = ({ points, onPointChange }: ILeafletMapCtrlProps) => {
               </FormGroup>
             </div>
             <div className="leaflet-map-slider">
-              <Slider
-                disabled={!isRadiusVisible}
-                value={radius}
-                step={10}
-                marks
-                min={10}
-                max={200}
-                valueLabelDisplay={"auto"}
-                onChange={(e, value) => {
-                  const updatedPoints = points.map((p) => ({
-                    ...p,
-                    radius: p.id === id ? (value as number) : p.radius,
-                  }));
-                  onPointChange(updatedPoints);
-                }}
-              />
+              {isRadiusVisible && (
+                <Slider
+                  value={radius}
+                  step={10}
+                  marks
+                  min={10}
+                  max={200}
+                  valueLabelDisplay={"auto"}
+                  onChange={(e, value) => {
+                    const updatedPoints = points.map((p) => ({
+                      ...p,
+                      radius: p.id === id ? (value as number) : p.radius,
+                    }));
+                    onPointChange(updatedPoints);
+                  }}
+                />
+              )}
             </div>
             {isRadiusVisible && <div>Radius: {radius} m</div>}
           </div>
         ),
       )}
-      {distance && <div>Distance: {distance.toString().substring(0, 7)} m</div>}
+      <div>
+        {distance && (
+          <div>Distance: {distance.toString().substring(0, 7)} m</div>
+        )}
+      </div>
+      <div>
+        <TextField
+          id={"sampling"}
+          label={"Sampling"}
+          type={"number"}
+          value={sampling}
+          onChange={(event) => setSampling(Number(event.target.value))}
+        />
+      </div>
       <LineChart
-        xAxis={[{ data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] }]}
+        xAxis={[{ data: xAxis }]}
         series={[
           {
             data: elevationProfile.map((elevation) => elevation.z),
